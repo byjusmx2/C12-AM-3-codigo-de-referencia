a059eb482bd84702f549ad2c087560b84adb6b2d
byjusmx2 <87038490+byjusmx2@users.noreply.github.com>
2021-09-30 11:23:48 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -53,14 +53,14 @@ function draw() {
   
   trex.collide(invisibleGround);
   
-  //spawn the clouds
+  //aparecer las nubes
   spawnClouds();
   
   drawSprites();
 }
 
 function spawnClouds() {
-  //write code here to spawn the clouds
+  //escribir aquí el código para aparecer las nubes
   if (frameCount % 60 === 0) {
     cloud = createSprite(600,100,40,10);
     cloud.addImage(cloudImage)
@@ -68,7 +68,7 @@ function spawnClouds() {
     cloud.scale = 0.4;
     cloud.velocityX = -3;
     
-    //adjust the depth
+    //ajustar la profundidad
     cloud.depth = trex.depth
     trex.depth = trex.depth + 1;
     }
